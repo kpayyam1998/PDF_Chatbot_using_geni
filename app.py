cb75fa5db93ee4ce33c609c1c51f15d8e6dff150
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-05-03 09:59:38 +0530

    code added

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import streamlit as st
+import base64
+st.title("File Extractor....")
+st.sidebar.title("File Details")
+# Handling the pdf files
+def display_pdf(file):
+    with open(file,"rb") as file_obj:
+        base64_pdf=base64.b64encode(file_obj.read()).decode("utf-8")
+    pdf_display = f'<iframe  src="data:application/pdf;base64,{base64_pdf}" width="100%" height="300"  type="application/pdf"></iframe>'
+    st.markdown(pdf_display,unsafe_allow_html=True)
+file_upload=st.sidebar.file_uploader("Upload file...",type=["pdf","docx"])
+if file_upload:
+    st.sidebar.success("File Uploaded successfully",icon="✅")
+    file_name=file_upload.name # file name
+    file_size=len(file_upload.read()) # get file size in bytes
+    st.sidebar.markdown("<h3 style=text-align:center>File Details</h3>",unsafe_allow_html=True)
+    file_details={
+        "file_name":file_name,
+        "file_size":file_size
+    }
+    st.sidebar.json(file_details) 
+    #save locally
+    file_path="docs/"+file_upload.name
+    with open(file_path,"wb") as f:
+        f.write(file_upload.getbuffer())
+    col1,col2=st.columns([1,2])
+    with col1:
+        st.markdown("<h3 style=text-align:center> PDF Preview</h3>",unsafe_allow_html=True)
+                #Display files
+        display_pdf(file_path)
+    with col2:
+        st.markdown("<h2 style=text-aligh:center> QA-Bot</h2>",unsafe_allow_html=True)
+        st.write("hello")
+else:
+    st.sidebar.error("Please upload a pdf/doc file",icon="🚨")
